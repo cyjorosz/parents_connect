3cc99ca645dc9da5d2be63d2e0c61a763934cc14
Carolyn Johnson Orosz <carolyn.johnson87@gmail.com>
2017-04-03 17:05:10 +0200

    created event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403145814) do
+ActiveRecord::Schema.define(version: 20170403150431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.text     "description"
+    t.string   "street_name"
+    t.string   "street_number"
+    t.string   "zipcode_string"
+    t.string   "city"
+    t.string   "country"
+    t.date     "date"
+    t.float    "duration"
+    t.integer  "profile_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["profile_id"], name: "index_events_on_profile_id", using: :btree
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "first_name"
@@ -51,5 +66,6 @@ ActiveRecord::Schema.define(version: 20170403145814) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "events", "profiles"
   add_foreign_key "profiles", "users"
 end
