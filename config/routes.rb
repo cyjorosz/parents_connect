29e4612c4b6acd330b38a58298819e0a3bbab143
Oliver Lehner <lehner.oliver@gmail.com>
2017-04-04 16:11:52 +0200

    added events controller with first methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users, :controllers => { registrations: "registrations"}
   root to: 'pages#home'
 
 
-  resources :profiles, only: [:show, :update, :destroy ] do 
+  resources :profiles do 
     resources :kids, only: [:create, :update, :destroy]
     resources :events, only: [:new, :edit, :create, :update, :destroy]
   end
