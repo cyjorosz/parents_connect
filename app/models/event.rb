937201d86823629ba4da5e82b4d1926f0918cc41
Carolyn Johnson Orosz <carolyn.johnson87@gmail.com>
2017-04-04 11:31:51 +0200

    added validations to event, kid and attendances model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :profile
+  has_many :attendances, dependent: :destroy
+  validates :start_date, presence: true
+  validates :description, presence: true
+  validates :street_name, presence: true
+  validates :street_number, presence: true
+  validates :city, presence: true
+  validates :zipcode, presence: true
+  validates :country, presence: true
 end
