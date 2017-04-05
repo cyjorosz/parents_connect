2ede1c045ae49e54e1d623d78d7b193e3fa19e9d
Oliver Lehner <lehner.oliver@gmail.com>
2017-04-05 16:07:44 +0200

    implementing active admin including necessary migrations etc.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404121139) do
+ActiveRecord::Schema.define(version: 20170405123621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.string   "author_type"
+    t.integer  "author_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+  end
 
   create_table "attendances", force: :cascade do |t|
     t.integer  "event_id"
@@ -22,6 +36,14 @@ ActiveRecord::Schema.define(version: 20170404121139) do
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_attendances_on_event_id", using: :btree
     t.index ["profile_id"], name: "index_attendances_on_profile_id", using: :btree
+  end
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "profile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_chat_rooms_on_profile_id", using: :btree
   end
 
   create_table "events", force: :cascade do |t|
@@ -84,24 +106,26 @@ ActiveRecord::Schema.define(version: 20170404121139) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin",                  default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "attendances", "events"
   add_foreign_key "attendances", "profiles"
+  add_foreign_key "chat_rooms", "profiles"
   add_foreign_key "events", "profiles"
   add_foreign_key "interest_links", "interests"
   add_foreign_key "interest_links", "profiles"
