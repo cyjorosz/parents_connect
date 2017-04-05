a103b0db7f19e3898556775cdc905648fc57206d
Oliver Lehner <lehner.oliver@gmail.com>
2017-04-05 16:47:53 +0200

    enable form on active admin user form

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -2,6 +2,15 @@ ActiveAdmin.register User do
 
   permit_params :email, :encrypted_password, :reset_password_token, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :created_at, :updated_at, :admin
 
+form do |f|
+    f.inputs "Identity" do
+      f.input :email
+    end
+    f.inputs "Admin" do
+      f.input :admin
+    end
+    f.actions
+  end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
