e73f2b0b29ae6fe87aa390265c6ec89a1a744446
Oliver Lehner <lehner.oliver@gmail.com>
2017-04-07 10:09:38 +0200

    fix signup issue with controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,7 +9,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    edit_profile_path(resource.profiles)
+    edit_profile_path(resource.profile)
   end
 
 end
