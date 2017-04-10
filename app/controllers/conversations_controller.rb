26a7eb31dbee1a2303e952e4c02d802d27041726
Oliver Lehner <lehner.oliver@gmail.com>
2017-04-10 11:50:21 +0200

    chat try commit not finished yet

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,6 +3,11 @@ class ConversationsController < ApplicationController
 
   layout false
 
+  def index
+    @profile = current_user.profile
+    @conversations 
+  end
+
   def create
     if Conversation.between(params[:sender_id],params[:recipient_id]).present?
       @conversation = Conversation.between(params[:sender_id],params[:recipient_id]).first
