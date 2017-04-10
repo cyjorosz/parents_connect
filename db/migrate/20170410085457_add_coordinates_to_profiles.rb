fba05dafa3585318176422f195b60f6b8b3e5076
Carolyn Johnson Orosz <carolyn.johnson87@gmail.com>
2017-04-10 11:50:24 +0200

    Added lat long columns to profiles

---
+++ db/migrate/20170410085457_add_coordinates_to_profiles.rb
@@ -0,0 +1,6 @@
+class AddCoordinatesToProfiles < ActiveRecord::Migration[5.0]
+  def change
+    add_column :profiles, :latitude, :float
+    add_column :profiles, :longitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406123030) do
+ActiveRecord::Schema.define(version: 20170410085457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,8 @@ ActiveRecord::Schema.define(version: 20170406123030) do
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
     t.string   "photo"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
